95fd322424d40de77ce1131adf3597ab1807fcf4
moaibad <fathulibad.412@gmail.com>
2023-09-01 00:26:30 +0700

    update login feature

--- reactjs/src/components/LoginCom.jsx
+++ reactjs/src/components/LoginCom.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 function LoginCom() {
-  return <>
+  const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new URLSearchParams();
+        formData.append('username', username);
+        formData.append('password', password);
+
+        try {
+            const response = await axios.post('http://localhost:8082/login', formData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+
+            if (response.data !== '' && response.data.constructor === Object) {
+              console.log(response.data)
+            }
+            else{
+              console.log("Invalid Username or Password")
+            }
+        } catch (error) {
+            // Tangani error, misalnya tampilkan pesan kesalahan
+            console.error('Terjadi kesalahan saat login:', error);
+        }
+    };
+
+  return (
     <section className="vh-100 bg-secondary login-background">
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
@@ -17,21 +47,23 @@ function LoginCom() {
                     hub
                   </div>
                 </div>
-                <div className="form-outline mb-4">
-                  <input type="email" id="typeEmailX-2" className="form-control form-control-lg" placeholder="Username" />
-                </div>
+                <form action="" id="login" method="post" onSubmit={handleSubmit}>
+                  <div className="form-outline mb-4">
+                    <input type="username" id="username" className="form-control form-control-lg" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required />
+                  </div>
 
-                <div className="form-outline mb-4">
-                  <input type="password" id="typePasswordX-2" className="form-control form-control-lg" placeholder="Password"/>
-                </div>
-                <button className="btn btn-primary btn-lg btn-block" type="submit"><b>Login</b></button>
+                  <div className="form-outline mb-4">
+                    <input type="password" id="password" className="form-control form-control-lg" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+                  </div>
+                  <button className="btn btn-primary btn-lg btn-block" type="submit"><b>Login</b></button>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  </>;
+  )
 }
 
 export default LoginCom;
--- spring-boot/src/main/java/com/mahasiswa/controller/AkunController.java
+++ spring-boot/src/main/java/com/mahasiswa/controller/AkunController.java
@@ -3,6 +3,7 @@ package com.mahasiswa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class AkunController {
     @Autowired
     AkunService akunService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public Akun login(@RequestParam String username, @RequestParam String password) {
         return akunService.login(username, password);
     }
